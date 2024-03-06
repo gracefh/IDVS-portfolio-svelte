38606f5cc4c2adf83334739b8ca964bf4ea08aa2
Grace Huang <gracefh@mit.edu>
2024-03-05 20:19:29 -0500

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -7,8 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: ''
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		}
 	}
 };
